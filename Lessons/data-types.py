7e10c71910b810b970d2c9cbdebd0650fe2f14a4
tferg12 <tferguson@unc.edu>
2024-05-16 19:59:27 -0400

    Finished Exercise 00! & Data types notes

---
+++ Lessons/data-types.py
@@ -0,0 +1,24 @@
+"""Objects, Types and Expressions Notes"""
+
+print("An example of numercial types:")
+
+print("An integer:")
+print(237)
+print(type(237))
+
+print("A float")
+print(1.25)
+
+print("Texual Types:")
+print("Helllo, World!")
+print(type("Hello, World!"))
+print("Hello, World!"[4])
+
+print("Booleans:")
+print(False)
+
+print("Changing types:")
+print(type(2.0))
+print(int(2.0))
+print(str(2.0))
+
